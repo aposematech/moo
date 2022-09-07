f117740d4dd6bbc1dd892d5547a6cc71573c2392
Jon Faviell <jon.faviell@gmail.com>
2022-09-07 14:18:58 -0500

    move to domain module

--- infra/modules/function-domain/main.tf
+++ infra/modules/function-domain/main.tf
@@ -17,3 +17,42 @@ resource "aws_route53domains_registered_domain" "registered_domain" {
 resource "aws_route53_zone" "zone" {
   name = var.registered_domain_name
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = "quotes.${var.registered_domain_name}"
+  validation_method = "DNS"
+
+  validation_option {
+    domain_name       = "quotes.${var.registered_domain_name}"
+    validation_domain = var.registered_domain_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
+resource "aws_route53_record" "certificate_validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.zone.id
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
+resource "aws_acm_certificate_validation" "certificate_validation" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.certificate_validation_record : record.fqdn]
+}
--- infra/modules/lambda-function/variables.tf
+++ infra/modules/lambda-function/variables.tf
@@ -15,15 +15,3 @@ variable "aws_account_number" {
   type        = string
   default     = ""
 }
-
-variable "registered_domain_name" {
-  description = "Route 53 Registered Domain Name"
-  type        = string
-  default     = ""
-}
-
-variable "hosted_zone_id" {
-  description = "Hosted Zone ID"
-  type        = string
-  default     = ""
-}
