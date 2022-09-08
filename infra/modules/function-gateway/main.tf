c7c4c7908f02e64a84cb1296b8c653d508bee81b
Jon Faviell <jon.faviell@gmail.com>
2022-09-07 19:11:12 -0500

    add route settings block

--- infra/modules/function-gateway/main.tf
+++ infra/modules/function-gateway/main.tf
@@ -18,12 +18,15 @@ resource "aws_apigatewayv2_api" "api" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_stage
 resource "aws_apigatewayv2_stage" "default_stage" {
-  api_id                 = aws_apigatewayv2_api.api.id
-  name                   = "default"
-  description            = "Managed by Terraform"
-  auto_deploy            = true
-  throttling_burst_limit = 1
-  throttling_rate_limit  = 1
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "default"
+  description = "Managed by Terraform"
+  auto_deploy = true
+
+  default_route_settings {
+    throttling_burst_limit = 1
+    throttling_rate_limit  = 1
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_integration
