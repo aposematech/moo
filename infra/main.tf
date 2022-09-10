8799fa299f1a2699f0cd5f0492924879fbb13c21
Jon Faviell <jon.faviell@gmail.com>
2022-09-09 19:38:20 -0500

    rename modules

--- infra/main.tf
+++ infra/main.tf
@@ -51,8 +51,8 @@ provider "checkly" {
   account_id = var.checkly_account_id
 }
 
-module "git_repo" {
-  source                  = "./modules/git-repo"
+module "git" {
+  source                  = "./modules/git"
   git_repo_name           = terraform.workspace
   git_repo_description    = var.git_repo_description
   git_repo_homepage_url   = "https://${var.api_subdomain_name}.${var.registered_domain_name}/${terraform.workspace}"
@@ -65,42 +65,42 @@ module "git_repo" {
   aws_region_value        = var.aws_region
 }
 
-module "image_repo" {
-  source          = "./modules/image-repo"
-  image_repo_name = module.git_repo.git_repo_name
+module "ecr" {
+  source        = "./modules/ecr"
+  ecr_repo_name = module.git.git_repo_name
 }
 
-module "lambda_function" {
-  source             = "./modules/lambda-function"
-  function_name      = module.image_repo.image_repo_name
+module "lambda" {
+  source             = "./modules/lambda"
+  function_name      = module.ecr.ecr_repo_name
   aws_region         = var.aws_region
   aws_account_number = var.aws_account_number
 }
 
-module "function_domain" {
-  source                 = "./modules/function-domain"
+module "domain" {
+  source                 = "./modules/domain"
   registered_domain_name = var.registered_domain_name
   api_subdomain_name     = var.api_subdomain_name
 }
 
-module "function_gateway" {
-  source                  = "./modules/function-gateway"
-  lambda_function_name    = module.lambda_function.lambda_function_name
-  lambda_function_arn     = module.lambda_function.lambda_function_arn
-  certificate_arn         = module.function_domain.certificate_arn
-  certificate_domain_name = module.function_domain.certificate_domain_name
-  hosted_zone_id          = module.function_domain.hosted_zone_id
+module "gateway" {
+  source                  = "./modules/gateway"
+  lambda_function_name    = module.lambda.lambda_function_name
+  lambda_function_arn     = module.lambda.lambda_function_arn
+  certificate_arn         = module.domain.certificate_arn
+  certificate_domain_name = module.domain.certificate_domain_name
+  hosted_zone_id          = module.domain.hosted_zone_id
   aws_region              = var.aws_region
   aws_account_number      = var.aws_account_number
 }
 
-module "function_monitors" {
-  source                       = "./modules/function-monitors"
+module "monitors" {
+  source                       = "./modules/monitors"
   aws_region                   = var.aws_region
-  registered_domain_name       = module.function_domain.registered_domain_name
-  certificate_domain_name      = module.function_domain.certificate_domain_name
-  hosted_zone_id               = module.function_domain.hosted_zone_id
-  api_gateway_name             = module.function_gateway.api_gateway_name
+  registered_domain_name       = module.domain.registered_domain_name
+  certificate_domain_name      = module.domain.certificate_domain_name
+  hosted_zone_id               = module.domain.hosted_zone_id
+  api_gateway_name             = module.gateway.api_gateway_name
   betteruptime_subdomain       = var.betteruptime_subdomain
   custom_status_page_subdomain = var.custom_status_page_subdomain
 }
