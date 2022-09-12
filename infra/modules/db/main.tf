2de73e50bc6bfbc77ee99701c855b70fd035505e
Jon Faviell <jon.faviell@gmail.com>
2022-09-12 18:45:57 -0500

    redesign table

--- infra/modules/db/main.tf
+++ infra/modules/db/main.tf
@@ -3,9 +3,15 @@ resource "aws_dynamodb_table" "db_table" {
   name         = var.db_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
+  range_key    = "sk"
 
   attribute {
     name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
     type = "S"
   }
 }
@@ -14,10 +20,17 @@ resource "aws_dynamodb_table" "db_table" {
 resource "aws_dynamodb_table_item" "lorem_ipsum" {
   table_name = aws_dynamodb_table.db_table.name
   hash_key   = aws_dynamodb_table.db_table.hash_key
+  range_key  = aws_dynamodb_table.db_table.range_key
 
   item = <<ITEM
 {
-    "${aws_dynamodb_table.db_table.hash_key}": {"S": "249806d3-96f9-4653-c93d-762f99e96102"}, "name": {"S": "Cicero"}, "quote": {"S": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod..."}
+  "${aws_dynamodb_table.db_table.hash_key}": {"S": "${var.db_table_name}"}, "${aws_dynamodb_table.db_table.range_key}": {"S": "${uuid()}"}, "value": {"M": {"name": {"S":"Cicero"}, "quote": {"S": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod..."}}}
 }
 ITEM
+
+  lifecycle {
+    ignore_changes = [
+      item,
+    ]
+  }
 }
