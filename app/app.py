4ebd75330bea7b5ee43f6950716e9ebc6e6606e6
Jon Faviell <jon.faviell@gmail.com>
2022-11-18 22:08:06 -0600

    use db table

--- app/app.py
+++ app/app.py
@@ -3,32 +3,24 @@
 # https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
 # https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html
 
+import os
 import random
 import json
-# import cowsay # https://pypi.org/project/cowsay # add to requirements.txt
 
 # https://awslabs.github.io/aws-lambda-powertools-python/latest/
-# from aws_lambda_powertools.utilities import parameters # add to requirements.txt
+from aws_lambda_powertools.utilities import parameters
 
-def random_quote(quote_file):
-    # quote_lines = open('file.txt').read().splitlines()
-    # quote = random.choice(quote_lines)
-    quote = next(quote_file)
-    for num, quote_line in enumerate(quote_file, 2):
-        if random.randrange(num):
-            continue
-        quote = quote_line
-    return quote.split('|')
+def random_quote():
+    dynamodb_provider = parameters.DynamoDBProvider(table_name=os.environ['DB_TABLE_NAME'])
+    quotes = dynamodb_provider.get_multiple(os.environ['DB_TABLE_HASH_KEY'])
+    return json.dumps(random.choice(list(quotes.items())))
 
 def handler(event, context):
-    quote = random_quote(open("quotes.txt"))
+    quote = json.loads(random_quote())
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps({
-            "quote": quote[1].strip(),
-            "name": quote[0]
-        })
+        "body": json.dumps(quote)
     }
