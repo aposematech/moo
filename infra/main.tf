020ac3eed0eea55549d4495513c84c3f601d5790
Jon Faviell <jon.faviell@gmail.com>
2022-11-19 22:13:19 -0600

    add topics

--- infra/main.tf
+++ infra/main.tf
@@ -70,6 +70,7 @@ module "git" {
   git_repo_name           = terraform.workspace
   git_repo_description    = var.git_repo_description
   git_repo_homepage_url   = "https://${var.api_subdomain_name}.${var.registered_domain_name}/${terraform.workspace}"
+  git_repo_topics         = ["api", "cowsay", "demo", "quotes"]
   git_repo_visibility     = var.git_repo_visibility
   aws_access_key_id_name  = "AWS_ACCESS_KEY_ID"
   aws_access_key_id_value = var.aws_access_key_id
