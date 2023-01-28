5b7746b4fa1a843cd2eae30f8d641439e1c217ab
Jon Faviell <jon.faviell@gmail.com>
2023-01-28 15:37:20 -0600

    add psf black pre-commit

--- app/app.py
+++ app/app.py
@@ -10,20 +10,19 @@ import json
 # https://awslabs.github.io/aws-lambda-powertools-python/latest/
 from aws_lambda_powertools.utilities import parameters
 
+
 def random_quote():
-    dynamodb_provider = parameters.DynamoDBProvider(table_name=os.environ['DB_TABLE_NAME'])
-    quotes = dynamodb_provider.get_multiple(os.environ['DB_TABLE_HASH_KEY'])
+    dynamodb_provider = parameters.DynamoDBProvider(
+        table_name=os.environ["DB_TABLE_NAME"]
+    )
+    quotes = dynamodb_provider.get_multiple(os.environ["DB_TABLE_HASH_KEY"])
     return json.dumps(random.choice(list(quotes.items())))
+
 
 def handler(event, context):
     quote = json.loads(random_quote())
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "quote": quote[1]["quote"],
-            "name": quote[1]["name"]
-        })
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"quote": quote[1]["quote"], "name": quote[1]["name"]}),
     }
