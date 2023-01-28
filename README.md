76191d3a3c33830e9e576c5674871d03f37149f5
Jon Faviell <jon.faviell@gmail.com>
2023-01-28 10:51:47 -0600

    update script and readme

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ $ sudo apt-get update
 $ sudo apt-get install -y uuid
 
 $ ./scripts/put-item.sh \
+-r "us-east-1" \
+-t "moo" \
 -n "Zarathustra" \
 -q "You must have chaos within you to give birth to a dancing star."
 ```
--- scripts/put-item.sh
+++ scripts/put-item.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-while getopts n:q: flag
+while getopts r:t:n:q: flag
 do
     case "${flag}" in
+        r) REGION=${OPTARG};;
+        t) TABLE=${OPTARG};;
         n) NAME=${OPTARG};;
         q) QUOTE=${OPTARG};;
     esac
 done
 
 aws dynamodb put-item \
---region us-east-1 \
---table-name moo \
---item '{"id": {"S": "moo"}, "sk": {"S": "'$(uuid)'"}, "value": {"M": {"name": {"S": "'"$NAME"'"}, "quote": {"S": "'"$QUOTE"'" }}}}'
+--region $REGION \
+--table-name $TABLE \
+--item '{"id": {"S": "'"$TABLE"'"}, "sk": {"S": "'$(uuid)'"}, "value": {"M": {"name": {"S": "'"$NAME"'"}, "quote": {"S": "'"$QUOTE"'" }}}}'
